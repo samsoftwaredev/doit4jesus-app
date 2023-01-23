8dfe3fcdb23fe2063295e91d20292e9fa6deeafd
Samuel Ruiz <samuelruiz@Samuels-MacBook-Pro.local>
2023-01-23 12:07:54 -0600

    initial volume value for slider

--- components/RosaryOptions/MusicVolumeOption/MusicVolumeOption.tsx
+++ components/RosaryOptions/MusicVolumeOption/MusicVolumeOption.tsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import { Stack, Slider } from "@mui/material";
 import VolumeDown from "@mui/icons-material/VolumeDown";
 import VolumeUp from "@mui/icons-material/VolumeUp";
+import { INITIAL_VOLUME } from "@/constants/mysteries";
 
 interface Props {
   onChange: Function;
 }
 
 const MusicVolumeOption = ({ onChange }: Props) => {
-  const [value, setValue] = useState<number>(100);
+  const [value, setValue] = useState<number>(INITIAL_VOLUME);
 
   const handleChange = (event: Event, newValue: number | number[]) => {
     setValue(newValue as number);
