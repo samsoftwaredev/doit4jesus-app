c6502091b34cfcff3fbd90ba6ab92c099ccffe8b
Samuel Ruiz <samuelruiz@Samuels-MacBook-Pro.local>
2023-05-17 16:31:01 -0500

    add trivia game

---
+++ pages/app/trivia-game.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import { ConfessionGuide } from "@/components";
+
+const TriviaGame: NextPage = () => {
+  return (
+    <>
+      <h1>TriviaGame</h1>
+    </>
+  );
+};
+
+export default TriviaGame;
