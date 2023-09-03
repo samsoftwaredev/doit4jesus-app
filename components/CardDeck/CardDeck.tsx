d17523d3b58127b04f52e0668e527fccd301eafa
Samuel Ruiz <samuelruiz@Samuels-MacBook-Pro.local>
2023-09-03 00:20:13 -0500

    track list of sins

--- components/CardDeck/CardDeck.tsx
+++ components/CardDeck/CardDeck.tsx
@@ -1,22 +1,23 @@
 import * as React from "react";
 import {
   Box,
-  Step,
-  StepContent,
   Button,
-  Paper,
   Typography,
   LinearProgress,
   Grid,
+  IconButton,
 } from "@mui/material";
 import { CardProps } from "@/interfaces/index";
-
+import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import CancelIcon from "@mui/icons-material/Cancel";
 interface Props {
   steps: CardProps[];
 }
 
 function CardDeck({ steps }: Props) {
   const [activeStep, setActiveStep] = React.useState(0);
+  const [sinsCommitted, setSinsCommitted] = React.useState<CardProps[]>([]);
   const progress = (activeStep / steps.length) * 100;
 
   const handleNext = () => {
@@ -29,44 +30,98 @@ function CardDeck({ steps }: Props) {
 
   const handleReset = () => {
     setActiveStep(0);
+    setSinsCommitted([]);
+  };
+
+  const handleSaveSin = (index: number) => {
+    handleNext();
+    setSinsCommitted([...sinsCommitted, steps[index]]);
+  };
+
+  const handleSkip = () => {
+    handleNext();
+  };
+
+  const handleFinishExam = () => {
+    setActiveStep(steps.length);
   };
 
   return (
-    <Box sx={{ maxWidth: 400 }}>
+    <Box>
       <LinearProgress variant="determinate" value={progress} />
       {steps
         .map((step, index) => (
-          <Box key={step.title}>
+          <Box key={step.title} my={2}>
             <Grid container height="250px" flexDirection={"column"}>
               <Typography variant="h5">{step.title}</Typography>
               <Typography>{step.question}</Typography>
             </Grid>
-            <Box sx={{ mb: 2 }}>
-              <Button
-                variant="contained"
-                onClick={handleNext}
-                sx={{ mt: 1, mr: 1 }}
-              >
-                {index === steps.length - 1 ? "Finish" : "Continue"}
-              </Button>
-              <Button
-                disabled={index === 0}
-                onClick={handleBack}
-                sx={{ mt: 1, mr: 1 }}
-              >
-                Back
-              </Button>
-            </Box>
+            <Grid container justifyContent="space-around">
+              <Grid md={4} item textAlign="center">
+                <IconButton
+                  disabled={index === 0}
+                  onClick={handleBack}
+                  style={{ transform: "scale(1.8)" }}
+                >
+                  <ArrowCircleLeftIcon />
+                </IconButton>
+              </Grid>
+              <Grid md={4} item textAlign="center">
+                <IconButton
+                  onClick={() => handleSaveSin(index)}
+                  style={{ transform: "scale(1.8)" }}
+                >
+                  <CheckCircleIcon />
+                </IconButton>
+              </Grid>
+              <Grid md={4} item textAlign="center">
+                <IconButton
+                  onClick={handleSkip}
+                  style={{ transform: "scale(1.8)" }}
+                >
+                  <CancelIcon />
+                </IconButton>
+              </Grid>
+              <Grid item my={5} md={12} textAlign="center">
+                <Button
+                  onClick={handleFinishExam}
+                  variant="outlined"
+                  size="small"
+                >
+                  Finish Exam
+                </Button>
+              </Grid>
+            </Grid>
           </Box>
         ))
         .slice(activeStep, activeStep + 1)}
       {activeStep === steps.length && (
-        <Paper square elevation={0} sx={{ p: 3 }}>
-          <Typography>All steps completed - you&apos;re finished</Typography>
-          <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+        <Box>
+          <Typography>
+            Examination of Conscience completed - you&apos;re finished
+          </Typography>
+
+          {sinsCommitted.length > 0 ? (
+            <Typography variant="h5">List of sins to confess:</Typography>
+          ) : (
+            <Typography variant="h5">
+              Great! No need to go to confession.
+            </Typography>
+          )}
+          {sinsCommitted.map((step, index) => (
+            <Grid px={2} py={1}>
+              <Typography variant="h6">{step.title}</Typography>
+              <Typography>{step.question}</Typography>
+            </Grid>
+          ))}
+          <Button
+            variant="outlined"
+            onClick={handleReset}
+            sx={{ mt: 1, mr: 1 }}
+          >
             Reset
           </Button>
-        </Paper>
+        </Box>
       )}
     </Box>
   );
