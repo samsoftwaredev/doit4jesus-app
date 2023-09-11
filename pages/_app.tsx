ba488212197aa5d1460bf25018bd689368ca1729
Samuel Ruiz <samuelruiz@Samuels-MacBook-Pro.local>
2023-09-10 22:52:21 -0500

    re-order styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,19 @@
 import type { AppProps } from "next/app";
-import { Roboto } from "@next/font/google";
-import { CssBaseline } from "@mui/material";
-import { ThemeProvider } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material";
 import { LanguageContextProvider } from "@/context/LanguageContext";
 import { ToastContextProvider } from "@/context/ToastContext";
 import { theme } from "@/styles/mui-overwrite";
 import "../styles/globals.scss";
 
-const roboto = Roboto({
-  weight: "400",
-  subsets: ["latin"],
-});
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ToastContextProvider>
-      <LanguageContextProvider>
-        <main className={roboto.className}>
-          <CssBaseline />
-          <ThemeProvider theme={theme}>
-            <Component {...pageProps} />
-          </ThemeProvider>
-        </main>
-      </LanguageContextProvider>
-    </ToastContextProvider>
+    <ThemeProvider theme={theme}>
+      <ToastContextProvider>
+        <LanguageContextProvider>
+          <Component {...pageProps} />
+        </LanguageContextProvider>
+      </ToastContextProvider>
+    </ThemeProvider>
   );
 }
 
