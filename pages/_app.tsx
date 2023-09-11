a2e35a861136e0b14cfb8169008a0817ca3f7d45
Samuel Ruiz <samuelruiz@Samuels-MacBook-Pro.local>
2023-09-10 22:04:33 -0500

    fix import ordering

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 import type { AppProps } from "next/app";
-import { CssBaseline } from "@mui/material";
 import { Roboto } from "@next/font/google";
-import "../styles/globals.scss";
+import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material";
 import { LanguageContextProvider } from "@/context/LanguageContext";
-import { theme } from "@/styles/mui-overwrite";
 import { ToastContextProvider } from "@/context/ToastContext";
+import { theme } from "@/styles/mui-overwrite";
+import "../styles/globals.scss";
 
 const roboto = Roboto({
   weight: "400",
