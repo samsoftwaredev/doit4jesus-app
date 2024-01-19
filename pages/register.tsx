0be6b670448a94dc92ee62e8c9c53de37ddede6e
Samuel Ruiz <samuelruiz@Samuels-MacBook-Pro.local>
2024-01-18 19:59:37 -0600

    skip registration if user is already logged-in

--- pages/register.tsx
+++ pages/register.tsx
@@ -2,15 +2,28 @@ import type { NextPage } from "next";
 import { MainLayout } from "@/layouts";
 import { HomeNavbar, Meta } from "../components";
 import { Box, Button, Container, Typography } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { LogIn, SignUp } from "../sections";
+import { useUserContext } from "@/context/UserContext";
+import { useRouter } from "next/router";
+import { NAV_APP_LINKS } from "../constants";
 
 const Register: NextPage = () => {
+  const navigate = useRouter();
+  const { user } = useUserContext();
   const [isNewUser, setIsNewUser] = useState(true);
+  const isNotAuth = user === null;
 
   const toggleView = () => {
     setIsNewUser(!isNewUser);
   };
+
+  useEffect(() => {
+    // if user is auth, navigate user to application
+    if (!isNotAuth) navigate.push(NAV_APP_LINKS.app.link);
+  }, []);
+
+  if (!isNotAuth) return null;
 
   return (
     <MainLayout topNavbar={<HomeNavbar />}>
