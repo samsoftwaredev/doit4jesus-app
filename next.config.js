14596ca966016d6488a4ff5480e9a62b901decb3
Samuel Ruiz <samuelruiz@Samuels-MacBook-Pro.local>
2024-04-28 17:43:03 -0500

    exclude supabase from edge functions

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
     includePaths: [path.join(__dirname, "styles")],
     prependData: `@import "@/styles/variable.scss";`,
   },
+  exclude: ["supabase"],
   webpack(config, options) {
     const { isServer } = options;
     config.module.rules.push({
