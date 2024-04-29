73ed742695dd083a512321b265ba8dfcf14421e0
Samuel Ruiz <samuelruiz@Samuels-MacBook-Pro.local>
2024-04-28 21:34:33 -0500

    send email with resend

--- supabase/functions/resend/index.ts
+++ supabase/functions/resend/index.ts
@@ -1,13 +1,28 @@
-console.log("Hello from Functions!");
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
 
 Deno.serve(async (req) => {
-  const { name } = await req.json();
-  const data = {
-    message: `Hello ${name}!`,
-  };
+  const res = await fetch("https://api.resend.com/emails", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${Deno.env.get("RESEND_API_KEY")}`,
+    },
+    body: JSON.stringify({
+      from: "DoIt4Jesus <team@doit4jesus.com>",
+      to: ["gero@fastmail.com"],
+      subject: "hello world",
+      html: "<strong>it works!</strong>",
+    }),
+  });
 
+  const data = await res.json();
   return new Response(JSON.stringify(data), {
-    headers: { "Content-Type": "application/json" },
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
   });
 });
 
@@ -17,7 +32,7 @@ Deno.serve(async (req) => {
   2. Make an HTTP request:
 
   curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/resend' \
-    --header 'Authorization: Bearer ' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
     --header 'Content-Type: application/json' \
     --data '{"name":"Functions"}'
 
