0cc9bf3a1628608bc56a5b970ace7672b6f81647
Samuel Ruiz <samuelruiz@Samuels-MacBook-Pro.local>
2026-03-11 18:13:24 -0500

    mock next router

--- __test__/jest.setup.js
+++ __test__/jest.setup.js
@@ -5,3 +5,16 @@ jest.mock('@vercel/analytics', () => ({
   track: jest.fn(),
   Analytics: jest.fn(),
 }));
+
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      push: jest.fn(),
+      prefetch: jest.fn(),
+      route: '/',
+      pathname: '',
+      query: {},
+      asPath: '',
+    };
+  },
+}));
